5b98e022b4ba6e702520df54f6cee326e935644b
tom_glb <84129849+Di-viner@users.noreply.github.com>
2022-02-19 11:30:01 +0800

    submit homework1

---
+++ Homework1/task1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace myApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double x, y;
+            char op;
+            string str;
+
+            Console.WriteLine("Please input two numbers");
+            str = Console.ReadLine();
+            string[] nums = str.Split(' ');
+            x = Double.Parse(nums[0]);
+            y = Double.Parse(nums[1]);
+
+            Console.WriteLine("Please input an operator");
+            op = Char.Parse(Console.ReadLine());
+            switch (op)
+            {
+                case '+':
+                    Console.WriteLine($"x + y: {x + y}");
+                    break;
+                case '-':
+                    Console.WriteLine($"x - y: {x - y}");
+                    break;
+                case '*':
+                    Console.WriteLine($"x * y: {x * y}");
+                    break;
+                case '/':
+                    Console.WriteLine($"x / y: {x / y}");
+                    break;
+                case '%':
+                    Console.WriteLine($"x % y: {x % y}");
+                    break;
+            }
+        }
+    }
+}
---
+++ Homework1/task2/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            char op;
+            double x, y;
+            x = Double.Parse(textBox1.Text);
+            y = Double.Parse(textBox2.Text);
+            op = Char.Parse(comboBox1.SelectedItem.ToString());
+            switch (op)
+            {
+                case '+':
+                    label1.Text = (x + y).ToString();
+                    break;
+                case '-':
+                    label1.Text = (x - y).ToString();
+                    break;
+                case '*':
+                    label1.Text = (x * y).ToString();
+                    break;
+                case '/':
+                    label1.Text = (x / y).ToString();
+                    break;
+                case '%':
+                    label1.Text = (x % y).ToString();
+                    break;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
